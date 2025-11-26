3720db59be221b0bc469a861dc1e40d1cffe1faf
Elnara Malikzade <12728@holbertonstudents.com>
2025-11-26 16:30:37 +0000

    Fix white space around modulo in 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    ones = i%10
-    tens = (i//10)%10
+    ones = i % 10
+    tens = (i // 10) % 10
     if ones <= tens:
         continue
     if i != 89:
