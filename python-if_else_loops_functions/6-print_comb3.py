1204e67c464f3bb347597cef3aea01e7ca371f98
Elnara Malikzade <12728@holbertonstudents.com>
2025-11-26 16:26:46 +0000

    Fix string format in 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,8 +4,7 @@ for i in range(0, 100):
     tens = (i//10)%10
     if ones <= tens:
         continue
-    if i == 89:
-        print(f"{i:02}", end='')
+    if i != 89:
+        print("{:02}, ".format(i), end='')
     else:
-        print(f"{i:02}", end=', ')
-print()
+        print("{:02}".format(i))
