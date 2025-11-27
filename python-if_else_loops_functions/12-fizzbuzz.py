ff3d8d701edb455271cd0df75db6f025cbe250d9
Elnara Malikzade <12728@holbertonstudents.com>
2025-11-27 07:38:00 +0000

    Fix for correct output in 12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 100):
-        if i % 3 == 0:
-            print("Fizz ")
+        if i % 3 == 0 AND i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
         elif i % 5 == 0:
-            print("Buzz ")
+            print("Buzz")
         else:
-            print(f"{i} ")
+            print(f"{i}")
