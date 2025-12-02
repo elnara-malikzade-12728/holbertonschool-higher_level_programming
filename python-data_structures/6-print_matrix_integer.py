3d0b141c3bb6f7d64d5cbcc57eee6af796165560
Elnara Malikzade <12728@holbertonstudents.com>
2025-12-02 16:51:18 +0000

    Fix of code in 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for col in row:
-            print(col, end=" ")
-        print("$")
+        for i in range(len(row)):
+            if i != 0:
+                print(" ", end="")
+            print("{:d}".format(row[i]), end="")
+        print()
