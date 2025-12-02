e2609218be35d5e55fa0e5ccb4bc4bcc777613ee
Elnara Malikzade <12728@holbertonstudents.com>
2025-12-02 04:16:17 +0000

    Fixed the code in 3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add
-import sys
 if __name__ == "__main__":
-    argv = sys.argv[0:]
-    count = len(argv)
-    for i in range(count):
-        print("{} ".format(int(add(i, i))))
+    import sys
+
+    total = 0
+    for arg in sys.argv[1:]:
+        total += int(arg)
+    print(total)
