b7cb01437a60894dd29368aa372731b63940e90a
Elnara Malikzade <12728@holbertonstudents.com>
2025-12-02 04:00:27 +0000

    Fix of the code in 3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+from calculator_1 import add
 import sys
 if __name__ == "__main__":
     argv = sys.argv[0:]
     count = len(argv)
     for i in range(count):
-        print("{} ".format(int(i)))
+        print("{} ".format(int(add(i, i))))
