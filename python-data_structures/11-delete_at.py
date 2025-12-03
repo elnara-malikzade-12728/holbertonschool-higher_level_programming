b10fbd5ee68d1738fd4234b5f6a6671ba1276229
Elnara Malikzade <12728@holbertonstudents.com>
2025-12-03 18:06:15 +0000

    Fixed the code in 11-delete_at.py

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    new_list = my_list.copy()
-    if idx < 0 or idx >= len(new_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
-    del new_list[idx]
-    return new_list            
-            
-         
-       
+    del my_list[idx]
+    return my_list
