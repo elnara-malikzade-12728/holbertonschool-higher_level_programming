53b61761c6ecd89bb434c80ccac4acb94108d990
Elnara Malikzade <12728@holbertonstudents.com>
2025-12-10 08:11:36 +0000

    Add 3-safe_print_division.py to python-exceptions

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+        print("Inside result: {}".format(result))
+    except ZeroDivisionError:
+        print("Inside result: {}".format(result))
+    finally:
+        return result  
