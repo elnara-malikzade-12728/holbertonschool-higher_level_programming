7e3de3f082c75c0ce83b3b9137ee6a06a8188c28
Elnara Malikzade <12728@holbertonstudents.com>
2025-12-10 08:18:28 +0000

    Add exceptions in 3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
         print("Inside result: {}".format(result))
-    except ZeroDivisionError:
+    except (ZeroDivisionError, ValueError, TypeError):
         print("Inside result: {}".format(result))
     finally:
-        return result  
+        return result
