d69a9c3797b3e01a2bdea09b9cd8dc6206fe071e
Elnara Malikzade <12728@holbertonstudents.com>
2025-12-15 18:50:48 +0000

    Fix the pycodestyle error of 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,8 +8,8 @@ class Square:
     def __init__(self, size=0):
         """Initializes a square with an optional size."""
 
-        self.__size = size
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
