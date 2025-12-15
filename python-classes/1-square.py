df210e12cb66824f93005e62bded0d131d5c82c1
Elnara Malikzade <12728@holbertonstudents.com>
2025-12-15 18:10:16 +0000

    Fix blank line in 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines a Square class."""
 
+
 class Square:
     """Represents a square."""
-
 
     def __init__(self, size):
         """Initializes a square with a private variable size."""
