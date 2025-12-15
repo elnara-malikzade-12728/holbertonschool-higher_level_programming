f99056ab9fab233b08fcb83519ab2dc3ea664496
Elnara Malikzade <12728@holbertonstudents.com>
2025-12-15 18:38:12 +0000

    Add 2-square.py to python-classes

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initializes a square with an optional size."""
+
+        self.size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >=0")
