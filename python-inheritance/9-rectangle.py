28de1a716898c001fe1d95a938624d20b8ab86db
Elnara Malikzade <12728@holbertonstudents.com>
2025-12-25 11:56:04 +0000

    Fix pep8 validation error

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -16,7 +16,8 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """Returns customized rectangle description."""
-        r = "[{}] {}/{}".format(__class__.__name__, self.__width, self.__height)
+        name = __class__.__name__
+        r = "[{}] {}/{}".format(name, self.__width, self.__height)
         return r
 
     def area(self):
