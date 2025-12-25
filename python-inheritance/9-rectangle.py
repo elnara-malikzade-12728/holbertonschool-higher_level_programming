b60c07633f659349a30ffa966c6939c844f39f94
Elnara Malikzade <12728@holbertonstudents.com>
2025-12-25 11:23:15 +0000

    Add 9-main.py and 9-rectangle.py

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class based on BaseGeometry class."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represents Rectangle class that inherits from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initializes private width and height."""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """Returns customized rectangle description."""
+        return "[{}] {}/{}".format(__class__.__name__, self.__width, self.__height)
+
+    def area(self):
+        """Method must be implemented."""
+        return self.__width * self.__height
