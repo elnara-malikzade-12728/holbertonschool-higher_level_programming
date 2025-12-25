337efe07b09c2165e968dcf3044aa6d404ba1cbe
Elnara Malikzade <12728@holbertonstudents.com>
2025-12-25 13:47:34 +0000

    Add 11-main.py and 11-square.py to python-inheritance

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a Square class that inherits from Rectangle class."""
+
+    def __init__(self, size):
+        """Initiates a constructor with private attribute size."""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Returns customized square class description."""
+        name = __class__.__name__
+        r = "[{}] {}/{}".format(name,self.__size, self.__size)
+        return r
