cff3064a35876dfb19692654fd4f40d34adc2098
Elnara Malikzade <12728@holbertonstudents.com>
2025-12-31 17:53:19 +0000

    Add 1-main.py, my_first_file.txt, 1-write_file.py to python-input_output

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a function that writes into the file."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file and returns the number of characters."""
+
+    with open(filename, "w", encoding="utf-8")as f:
+        return f.write(text)
+        
