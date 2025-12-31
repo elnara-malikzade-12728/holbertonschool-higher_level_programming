ad7738ca52ec87ce20bc3ac55bfd3959119f2bb5
Elnara Malikzade <12728@holbertonstudents.com>
2025-12-31 18:17:04 +0000

    Fix blank line error in 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,7 +3,6 @@
  of a text file and returns the number of characters added."""
 
 
-
 def append_write(filename="", text=""):
     """Appends a string at the end of a text file
     and returns the number of characters added."""
