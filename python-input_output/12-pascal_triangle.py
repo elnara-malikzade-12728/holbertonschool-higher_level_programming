251ab582fa16f74d0d1e91f2dcfb2715a7f6a465
Elnara Malikzade <12728@holbertonstudents.com>
2026-01-01 17:04:36 +0000

    Fix 12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -9,16 +9,16 @@ def pascal_triangle(n):
 
     if n <= 0:
         return []
-    
-    triangle = [[1]] # Start with the first row
-    
+
+    triangle = [[1]]
+
     for i in range(1, n):
         prev_row = triangle[-1]
-        # New row starts with 1, adds sums of pairs, ends with 1
         curr_row = [1]
+
         for j in range(len(prev_row) - 1):
             curr_row.append(prev_row[j] + prev_row[j+1])
         curr_row.append(1)
         triangle.append(curr_row)
-        
+
     return triangle
