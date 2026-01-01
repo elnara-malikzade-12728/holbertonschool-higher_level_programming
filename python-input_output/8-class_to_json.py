fe7cdd1ef8199769f70034092733a81a6ed63802
Elnara Malikzade <12728@holbertonstudents.com>
2026-01-01 15:39:59 +0000

    Fix 8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """Defines a function that returns the dictionary description
  with simple data structure for JSON serialization of an object."""
-import json
 
 
 def class_to_json(obj):
     """Returns teh dictionary description with simple
  data structure (list, dictionary, string, integer and boolean)
  for JSON serialization of an object."""
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
