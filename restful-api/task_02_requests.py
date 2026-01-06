3d25c21464e4cd2da1b79f6cc2741eb821963059
Elnara Malikzade <12728@holbertonstudents.com>
2026-01-06 09:46:16 -0500

    Add main_02_requests.py task_02_requests.py

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,36 @@
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    """Fetches all post from JSONPlaceholder."""
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(url)
+    print(f"Status code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post.get('title'))
+
+def fetch_and_save_posts():
+    """Fetches all post from JSONPlaceholder and saves it to JSON."""
+
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+        structured_data = []
+        for post in posts:
+            structured_data.append({
+                'id': post.get('id'),
+                'title': post.get('title'),
+                'body': post.get('body')
+            })
+        with open('post_pcsv', 'w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(structured_data)
