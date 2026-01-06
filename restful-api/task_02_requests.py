0996e6792d29bd54d1ac6e2f76d08d03a0d65f31
Elnara Malikzade <12728@holbertonstudents.com>
2026-01-06 09:53:53 -0500

    Fix print output in task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,8 +6,7 @@ def fetch_and_print_posts():
     """Fetches all post from JSONPlaceholder."""
     url = 'https://jsonplaceholder.typicode.com/posts'
     response = requests.get(url)
-    print("Status code: 200")
-
+    print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
