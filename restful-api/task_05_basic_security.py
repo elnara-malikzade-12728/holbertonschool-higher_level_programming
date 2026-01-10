4c060c2adff148d859299ad7333d5c9b9466746c
Elnara Malikzade <12728@holbertonstudents.com>
2026-01-10 02:24:26 -0500

    Add task_05_basic_security.py

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,10 +1,7 @@
 import os
 from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
-from flask_jwt_extended import (
-    JWTManager, create_access_token, jwt_required,
-    get_jwt_identity, get_jwt
-)
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
