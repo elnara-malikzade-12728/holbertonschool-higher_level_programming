6da2fa5ce2eef1b1e327a78ba6b7d2bdfdd24994
Elnara Malikzade <12728@holbertonstudents.com>
2026-01-10 02:36:29 -0500

    Add missing files

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -38,6 +38,13 @@ def handle_invalid_token_error(err):
 def handle_expired_token_error(err):
     return jsonify({"error": "Token has expired"}), 401
 
+@jwt.revoked_token_loader
+def handle_revoked_token_error(err):
+      return jsonify({"error": "Token has been revoked"}), 401
+
+@jwt.needs_fresh_token_loader
+def handle_needs_fresh_token_error(err):
+      return jsonify({"error": "Fresh token required"}), 401
 # --- Routes ---
 
 # 1. Basic Protected Route
