9a4b093d399de349d95c8c3414e7e426bb1dc3e3
Elnara Malikzade <12728@holbertonstudents.com>
2026-01-13 09:22:57 +0000

    Add 16-no_link.sql

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,2 @@
 -- lists all records of the table excluding null
-INSERT INTO second_table (id, name, score) VALUES (5, 'Aria', 12), (6, 'Aria', 18);
-SELECT score, name FROM second_table WHERE name IS NOT NULL AND score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
