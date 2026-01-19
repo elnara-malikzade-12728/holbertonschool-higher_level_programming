aa356fd6393f964f64f36ffeff3d2bb0d5335980
Elnara Malikzade <12728@holbertonstudents.com>
2026-01-19 05:43:40 +0000

    Fix 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates a user with all privileges and sets a password
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL PRIVILEGES ON 1-create_user.* TO 'user_0d_1'@'localhost'; 
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'; 
