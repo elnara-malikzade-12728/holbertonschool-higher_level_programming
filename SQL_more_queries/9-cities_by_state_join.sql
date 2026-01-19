da4964980dbaf7605c981852db627fe8670f23a8
Elnara Malikzade <12728@holbertonstudents.com>
2026-01-19 07:59:33 +0000

    Fix 9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all the cities from the database
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.id = states.id;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id;
