8ddb82b4e674d98a9b5a7f0a58befe815ed1376c
elnara-malikzade-12728 <12728@holbertonstudents.com>
2026-01-31 18:22:06 +0000

    Fix 7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 
 const firstArg = process.argv[2];
 const number = parseInt(firstArg, 10);
-if (Number.isInteger(firstArg)) {
+if (Number.isInteger(number)) {
   let i = 0;
   while (i < number) {
     console.log('C is fun');
