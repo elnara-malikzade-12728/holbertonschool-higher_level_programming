9ed12b912d2b4897e81eb2a707754330f98a246f
elnara-malikzade-12728 <12728@holbertonstudents.com>
2026-01-31 17:55:00 +0000

    Fix 5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const firstArgument = process.argv[2];
 const number = parseInt(firstArgument, 10);
-if (number.isInteger) {
+if (Number.isInteger(number)) {
   console.log('My number: ' + number);
 } else {
   console.log('Not a number');
