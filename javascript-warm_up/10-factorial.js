666524d8760f8cd49fb6df4c4f9bcd22347b6682
Elnara Malikzade <12728@holbertonstudents.com>
2026-02-01 12:16:08 +0000

    Fix 10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 function factorial (number) {
-  if (isNaN(number) || number <=1) {
+  if (isNaN(number) || number <= 1) {
     return 1;
   } else {
-    return number * factorial(number - 1);	  
+    return number * factorial(number - 1);
   }
 }
 
